335045d7e9185d1002729696f6db1da7c3882c15
Jose Felipe <josefelipeas1@gmail.com>
2026-03-27 18:02:26 -0400

    feat(components/ui): add button component

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,13 @@
+import "./Button.css";
+
+function Button({ children, outline, className = "", ...props }) {
+  const classes = "btn " + className + (outline ? " btn-outline" : "");
+
+  return (
+    <button className={classes} {...props}>
+      <span className="btn-text">{children}</span>
+    </button>
+  );
+}
+
+export default Button;
