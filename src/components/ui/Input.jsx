44c809c9b33d6eb664942ba922b45b67ce1d368c
Jose Felipe <josefelipeas1@gmail.com>
2026-03-30 13:49:37 -0400

    feat:components/ui: Add input component Update button component

---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,24 @@
+import "./Input.css";
+
+function Input(props) {
+  const { placeholder, className, required, type = "text", ...rest } = props;
+  const classes = "input " + (className ? className : "");
+
+  return (
+    <label className="label">
+      {placeholder}
+      {required && <span className="required">*</span>}
+      <div>
+        <input
+          className={classes}
+          placeholder={placeholder}
+          required={required}
+          type={type}
+          {...rest}
+        ></input>
+      </div>
+    </label>
+  );
+}
+
+export default Input;
