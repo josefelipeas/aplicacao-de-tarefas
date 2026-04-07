c87f40287400b598a85de3db218b5fd4c749b1f4
Jose Felipe <josefelipeas1@gmail.com>
2026-04-07 16:58:04 -0400

    feat(pages/Addtarefa): Adjust inline form - Altered form design - Updated CSS for better UI experience - Added form CSS for mobile devices

--- src/pages/AddTarefas.jsx
+++ src/pages/AddTarefas.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import "./AddTarefas.css";
-import Button from "../components/ui/Button"; 
+import Button from "../components/ui/Button";
 import Input from "../components/ui/Input";
 
 function AddTarefas() {
   return (
     <div>
-      <h2>Adicione uma tarefa</h2>
       <form className="task-form">
-        <Input label="Descrição" placeholder="Descrição" required />
-        <label htmlFor="categoria" className="label">
-          Categoria
-          <span className="required">*</span>
-        </label>
-        <select id="categoria">
-          <option value="">Selecione uma categoria</option>
-          <option value="trabalho">Trabalho</option>
-          <option value="pessoal">Pessoal</option>
-          <option value="estudo">Estudo</option>
-        </select>
-        <Button type="submit">Adicionar Tarefa</Button>
+        <div className="form-field">
+          <label htmlFor="descricao" className="label">
+            Descrição
+            <span className="required">*</span>
+          </label>
+          <input id="descricao" className="input" placeholder="Descrição" required />
+        </div>
+        <div className="form-field">
+          <label htmlFor="categoria" className="label">
+            Categoria
+            <span className="required">*</span>
+          </label>
+          <select id="categoria">
+            <option value="">Selecione uma categoria</option>
+            <option value="trabalho">Trabalho</option>
+            <option value="pessoal">Pessoal</option>
+            <option value="estudo">Estudo</option>
+          </select>
+        </div>
+        <Button outline type="submit">Adicionar Tarefa</Button>
       </form>
     </div>
   );
