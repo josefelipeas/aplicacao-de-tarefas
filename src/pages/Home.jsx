edaf845de637879f103de1af1cf626d891e61750
Jose Felipe <josefelipeas1@gmail.com>
2026-04-08 10:05:42 -0400

    pages/home: Adjusting h1 text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div className="home-layout">
       <img src="/src/assets/banner.jpg" alt="Banner" className="home-banner" />
-      <h1>Bem-vindo ao Task Manager</h1>
+      <h1>Bem-vindo ao seu Gerenciador de Tarefas</h1>
       <p>Organize suas tarefas de forma eficiente e fácil de usar.</p>
       <Link to="/add-tasks" className="home-link">
         <Button>Adicionar Tarefas</Button>
